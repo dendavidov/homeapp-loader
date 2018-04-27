261a49990ccbbf6da3c0f066d10fa11603a10fe2
denis <mail@dendavidov.com>
2018-04-27 16:56:44 +0300

    Add auto hide

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './main-loader.css';
 
 // STATIC_ASSETS
 
+const LOADER_AUTO_HIDE_TIMEOUT = 600000;
+
 const mainLoader = new MainLoader();
 
 mainLoader.show();
 
 window.MAIN_LOADER = mainLoader;
+
+setTimeout(() => mainLoader.hide, LOADER_AUTO_HIDE_TIMEOUT);
